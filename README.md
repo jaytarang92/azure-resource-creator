80572bf0a5a2c39709534c1f504c9935bdb72fbc
Jasmit Tarang <j_hack92@Jasmits-iMac.local>
2020-02-09 02:36:49 -0600

    add service principal handler

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # azure-resource-creator
 Python application that creates azure resources
+
+## Requirements
+1. Azure Service Principal
+2. Azure Cli
+3. Python 3.*
+
+## Getting Started
+1. Login to Azure using the CLI : ``az login``
+2. Create the service principal : ``az ad sp create-for-rbac --name <$NAME-HERE>-service-principal``
+
---
+++ src/libs/service_principal.py
@@ -0,0 +1,31 @@
+from azure.common.credentials import ServicePrincipalCredentials
+from json import loads
+
+
+class ServicePrincipal(object):
+
+    def __init__(self, sp_config_dict):
+        self.client_id = sp_config_dict['appId']
+        self.secret = sp_config_dict['password']
+        self.subscription_id = sp_config_dict.get('subscription_id')
+        self.tenant = sp_config_dict['tenant']
+
+
+class ServicePrincipalCreator:
+
+    def __init__(self, path_to_sp_config='../service-principal.json'):
+        self.path_to_sp_config = path_to_sp_config
+
+    def create_sp_object(self):
+        try:
+            with open(self.path_to_sp_config, 'r') as sp_config_file:
+                _service_principal = ServicePrincipal(loads(sp_config_file.read()))
+                return _service_principal
+        except IOError:
+            raise FileExistsError(f'{self.path_to_sp_config}: file does not exist.')
+
+    def create_sp_from_credentials(self, sp_object=None):
+        if sp_object is None:
+            sp_object = self.create_sp_object()
+        return ServicePrincipalCredentials(client_id=sp_object.client_id, secret=sp_object.secret,
+                                           tenant=sp_object.tenant)
